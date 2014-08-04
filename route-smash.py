637910a99830ef7e35df0c5f26a21d80804b16a7
dfex <Ben Dale>
2014-08-04 18:59:41 +1000

    Updated comments

--- route-smash.py
+++ route-smash.py
@@ -13,6 +13,7 @@ for first in range(1,223):
 		for third in range(0,255):
 			sys.stdout.write('announce route %d.%d.%d.0/24 next-hop 10.0.0.31\n' % (first, second, third))
 			sys.stdout.flush()
+			## Back off timer if router is too slow:
 			##time.sleep(0.001)
 
 while True:
